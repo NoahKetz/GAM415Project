6a59b9343ffadbe442ec63895612718a3142b5d3
Noah Ketzenberger <noah.ketzenberger@snhu.edu>
2026-09-13 22:24:54 -0400

    Completed Stepping Stone One projectile and splat decal system

--- Source/GAM415Project/GAM415ProjectProjectile.cpp
+++ Source/GAM415Project/GAM415ProjectProjectile.cpp
@@ -1,42 +1,134 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GAM415ProjectProjectile.h"
+
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Materials/MaterialInterface.h"
+#include "Materials/MaterialInstanceDynamic.h"
 
-AGAM415ProjectProjectile::AGAM415ProjectProjectile() 
+
+AGAM415ProjectProjectile::AGAM415ProjectProjectile()
 {
-	// Use a sphere as a simple collision representation
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(5.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &AGAM415ProjectProjectile::OnHit);		// set up a notification for when this component hits something blocking
+	CollisionComp->OnComponentHit.AddDynamic(this, &AGAM415ProjectProjectile::OnHit);
 
-	// Players can't walk on it
-	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	CollisionComp->SetWalkableSlopeOverride(
+		FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f)
+	);
+
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
-
-	// Set as root component
 	RootComponent = CollisionComp;
 
-	// Use a ProjectileMovementComponent to govern this projectile's movement
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	// Visible projectile mesh created through C++.
+	BallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BallMesh"));
+	BallMesh->SetupAttachment(CollisionComp);
+	BallMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	ProjectileMovement =
+		CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+
 	ProjectileMovement->UpdatedComponent = CollisionComp;
 	ProjectileMovement->InitialSpeed = 3000.f;
 	ProjectileMovement->MaxSpeed = 3000.f;
 	ProjectileMovement->bRotationFollowsVelocity = true;
 	ProjectileMovement->bShouldBounce = true;
 
-	// Die after 3 seconds by default
 	InitialLifeSpan = 3.0f;
 }
 
-void AGAM415ProjectProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+
+void AGAM415ProjectProjectile::BeginPlay()
 {
-	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
+	Super::BeginPlay();
+
+	// Generate one color that will be shared by the projectile and decal.
+	RandColor = FLinearColor(
+		FMath::FRandRange(0.0f, 1.0f),
+		FMath::FRandRange(0.0f, 1.0f),
+		FMath::FRandRange(0.0f, 1.0f),
+		1.0f
+	);
+
+	if (ProjectileMaterial && BallMesh)
 	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+		ProjectileDMI =
+			UMaterialInstanceDynamic::Create(ProjectileMaterial, this);
+
+		if (ProjectileDMI)
+		{
+			BallMesh->SetMaterial(0, ProjectileDMI);
+
+			ProjectileDMI->SetVectorParameterValue(
+				TEXT("ProjColor"),
+				RandColor
+			);
+		}
+	}
+}
+
+
+void AGAM415ProjectProjectile::OnHit(
+	UPrimitiveComponent* HitComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse,
+	const FHitResult& Hit
+)
+{
+	if ((OtherActor != nullptr) &&
+		(OtherActor != this) &&
+		(OtherComp != nullptr))
+	{
+		if (SplatMaterial)
+		{
+			// Randomly chooses one of the four splat frames.
+			const float FrameNumber =
+				static_cast<float>(FMath::RandRange(0, 3));
+
+			UDecalComponent* Decal =
+				UGameplayStatics::SpawnDecalAtLocation(
+					GetWorld(),
+					SplatMaterial,
+					FVector(20.0f, 40.0f, 40.0f),
+					Hit.ImpactPoint + Hit.ImpactNormal * 1.0f,
+					Hit.ImpactNormal.Rotation(),
+					0.0f
+				);
+
+			if (Decal)
+			{
+				UMaterialInstanceDynamic* DecalDMI =
+					Decal->CreateDynamicMaterialInstance();
+
+				if (DecalDMI)
+				{
+					// Matches the decal color to the projectile color.
+					DecalDMI->SetVectorParameterValue(
+						TEXT("Color"),
+						RandColor
+					);
+
+					DecalDMI->SetScalarParameterValue(
+						TEXT("Frame"),
+						FrameNumber
+					);
+				}
+			}
+		}
+
+		if (OtherComp->IsSimulatingPhysics())
+		{
+			OtherComp->AddImpulseAtLocation(
+				GetVelocity() * 100.0f,
+				GetActorLocation()
+			);
+		}
 
 		Destroy();
 	}
--- Source/GAM415Project/GAM415ProjectProjectile.h
+++ Source/GAM415Project/GAM415ProjectProjectile.h
@@ -8,30 +8,64 @@
 
 class USphereComponent;
 class UProjectileMovementComponent;
+class UStaticMeshComponent;
+class UMaterialInterface;
+class UMaterialInstanceDynamic;
 
-UCLASS(config=Game)
+UCLASS(config = Game)
 class AGAM415ProjectProjectile : public AActor
 {
 	GENERATED_BODY()
 
 	/** Sphere collision component */
-	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
 	USphereComponent* CollisionComp;
 
-	/** Projectile movement component */
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovement;
 
+	/** Visible projectile mesh created through C++ */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Projectile, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BallMesh;
+
+
+	UPROPERTY(EditDefaultsOnly, Category = Materials)
+	UMaterialInterface* ProjectileMaterial;
+
+	/** Material used to create the splatter decal */
+	UPROPERTY(EditDefaultsOnly, Category = Materials)
+	UMaterialInterface* SplatMaterial;
+
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* ProjectileDMI;
+
+	/** Stores one random color so the projectile and decal can share it */
+	FLinearColor RandColor;
+
+protected:
+
+	
+	virtual void BeginPlay() override;
+
 public:
+
 	AGAM415ProjectProjectile();
 
-	/** called when projectile hits something */
+	/** Called when projectile hits something */
 	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnHit(
+		UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+	);
 
-	/** Returns CollisionComp subobject **/
+	/** Returns CollisionComp subobject */
 	USphereComponent* GetCollisionComp() const { return CollisionComp; }
-	/** Returns ProjectileMovement subobject **/
+
+	/** Returns ProjectileMovement subobject */
 	UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
 };
-
